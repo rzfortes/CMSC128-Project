ed4ec224b1205b425ef4d7f9ead2c813b2f77287
rzfortes <rzfortes@up.edu.ph>
2017-10-09 18:59:34 +0800

    rica commented

--- src/CPADirectoryMain.java
+++ src/CPADirectoryMain.java
@@ -1,3 +1,5 @@
+package src;
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -5,6 +7,8 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+// i rica commented
 
 public class CPADirectoryMain extends Application {
 
