39c22504725e5d72bd13fc91ceb508df59b37f76
rzfortes <rzfortes@up.edu.ph>
2017-10-12 22:39:44 +0800

    simple event handling for buttons

--- src/sample/Pier3Controller.java
+++ src/sample/Pier3Controller.java
@@ -1,16 +1,13 @@
 package sample;
 
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-//For Pier1
+//For Pier3
 
-public class Pier1 implements Initializable {
-
+public class Pier3Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("This should be loadingg the map ...");
