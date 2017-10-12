77c23a9d47fc8889ae55baf32b0381c48a411dbb
Danielle Hernandez <dmhernandez@up.edu.ph>
2017-10-12 10:38:46 +0800

    New Version with FXML

--- src/sample/Main2Controller.java
+++ src/sample/Main2Controller.java
@@ -5,6 +5,8 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+//Pier3
+
 public class Main2Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -5,6 +5,8 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+//Pier1
+
 public class MainController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
