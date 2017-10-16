46e08abe8b852be2b0d209686497d5a4105f256d
rmjfamat <rjfamat@up.edu.ph>
2017-10-16 09:37:10 +0700

    toilets animation

--- src/sample/PierController.java
+++ src/sample/PierController.java
@@ -1,12 +1,20 @@
 package sample;
 
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tooltip;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+import javafx.util.Duration;
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -27,7 +35,7 @@ public class PierController implements Initializable {
     public Button btn_prayer;
     public Button btn_preDepart;
     public Button btn_inspect;
-    public Button btn_toilets;
+    public Button btn_toilets1;
     public Button btn_terminalFee;
     public Button btn_waitingArea;
     public Button btn_gate1;
@@ -35,6 +43,9 @@ public class PierController implements Initializable {
     public Button btn_gate2;
     public Button btn_gate3;
     public Button btn_gate4;
+    public ImageView loc;
+    public Path path = new Path();
+    public PathTransition trans = new PathTransition();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -57,6 +68,21 @@ public class PierController implements Initializable {
         if(source instanceof Button) {
             Button clickedBtn = (Button) source; // gets the name of the clicked button
             System.out.println("You clicked " + clickedBtn.getText());
+
+        }
+
+        if(source == btn_toilets1){
+            path.setStrokeWidth(10);
+            path.setStroke(Color.BLACK);
+            path.getElements().add(new MoveTo(0, 0));
+            path.getElements().add(new LineTo(155, 0));
+            path.getElements().add(new LineTo(155, -35));
+
+            trans.setNode(loc);
+            trans.setDuration(Duration.seconds(5));
+            trans.setPath(path);
+            trans.setCycleCount(1);
+            trans.play();
 
         }
 
